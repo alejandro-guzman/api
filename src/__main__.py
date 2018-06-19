0707ecded984f8ffeed01eb96235bbc821dcebf0
Alejandro Guzman <a.guillermoguzman@gmail.com>
2018-06-18 23:07:24 -0400

    Adding pymongo, rabbitmq, and redis port

--- src/__main__.py
+++ src/__main__.py
@@ -3,24 +3,43 @@ import socket
 import subprocess
 
 from flask import Flask, jsonify
-import redis
+from redis import StrictRedis
+from pika import BlockingConnection, ConnectionParameters
+from pymongo import MongoClient
 
 
 app = Flask(__name__)
 
 redis_host = os.environ['REDIS_HOST']
-red = redis.StrictRedis(host=redis_host, port=6379, db=0)
-# r.set('foo', 'bar')
+redis_port = int(os.environ['REDIS_PORT'])
+red_client = StrictRedis(host=redis_host, port=redis_port, db=0)
 
+rabbitmq_host = os.environ['RABBITMQ_HOST']
+rabbitmq_conn = BlockingConnection(ConnectionParameters(host=rabbitmq_host))
+
+mongo_host = os.environ['MONGO_HOST']
+mongo_port = int(os.environ['MONGO_PORT'])
+mongo_conn = MongoClient(host=mongo_host, port=mongo_port)
+
+
+@app.route('/set/<key>/<value>')
+def _set(key, value):
+    red_client.set(key, value)
+    return jsonify({
+        'result': 'OK'
+    })
+
+@app.route('/get/<key>')
+def get(key):
+    return jsonify({
+        key: red_client.get(key).decode('utf-8')
+    })
 
 @app.route("/ping")
 def ping():
-    uptime = subprocess.call(['cat', '/proc/uptime'])
-    return jsonify({
-        'hostname': socket.gethostname(),
-        'uptime': uptime,
-        'redis.foo': red.get('foo').decode('utf-8')
-    })
+    print('hello')
+    return jsonify(dict(hostname=socket.gethostname(),
+                        uptime=subprocess.call(['cat', '/proc/uptime'])))
 
 
 app.run(host='0.0.0.0', port=5000, debug=True)
