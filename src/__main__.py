7b2a754336a3a9bd74c32b15a2219b30cb3abb99
Alejandro Guzman <a.guillermoguzman@gmail.com>
2018-06-19 22:44:47 -0400

    include management console and add producer endpoint to rabbitmq

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,8 @@
 import os
 import socket
 import subprocess
+import time
+import json
 
 from flask import Flask, jsonify
 from redis import StrictRedis
@@ -16,17 +18,35 @@ red_client = StrictRedis(host=redis_host, port=redis_port, db=0)
 
 rabbitmq_host = os.environ['RABBITMQ_HOST']
 rabbitmq_conn = BlockingConnection(ConnectionParameters(host=rabbitmq_host))
+rmq_channel = rabbitmq_conn.channel()
+rmq_channel.queue_declare(queue='job')
 
 mongo_host = os.environ['MONGO_HOST']
 mongo_port = int(os.environ['MONGO_PORT'])
 mongo_conn = MongoClient(host=mongo_host, port=mongo_port)
 
 
+@app.route('/produce/<message>')
+def produce(message):
+    # Create message
+    body = json.dumps({
+        'created': time.ctime(),
+        'message': message
+    })
+
+    try:
+        rmq_channel.basic_publish(exchange='', routing_key='job', body=body)
+        resp = dict(result='OK')
+    except Exception as e:
+        resp = dict(result='ERR', details=str(e))
+
+    return jsonify(resp)
+
 @app.route('/set/<key>/<value>')
 def _set(key, value):
     red_client.set(key, value)
     return jsonify({
-        'result': 'OK'
+        'result': 'OKAY'
     })
 
 @app.route('/get/<key>')
