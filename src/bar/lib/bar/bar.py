e754738a8ee0ed4d2fa11d74c0993a977476eba4
Alejandro Guzman <a.guillermoguzman@gmail.com>
2018-06-30 00:14:31 -0400

    got a round trip going on

--- src/bar/lib/bar/bar.py
+++ src/bar/lib/bar/bar.py
@@ -1,6 +1,9 @@
+import logging
 import os
+import sys
 
 from pika import BlockingConnection, ConnectionParameters
+from pymongo import MongoClient
 
 
 rabbitmq_host = os.environ['RABBITMQ_HOST']
@@ -8,12 +11,22 @@ rabbitmq_conn = BlockingConnection(ConnectionParameters(host=rabbitmq_host))
 rmq_channel = rabbitmq_conn.channel()
 rmq_channel.queue_declare(queue='job')
 
+client = MongoClient('db')
+
+log = logging.getLogger(__name__)
+
+stdout_handler = logging.StreamHandler(sys.stdout)
+stdout_handler.setFormatter(logging.Formatter('%(asctime)s %(message)s'))
+stdout_handler.setLevel(logging.DEBUG)
+log.addHandler(stdout_handler)
+log.setLevel(logging.DEBUG)
+
 
 def handler(ch, method, properties, body):
-    print(" [x] Received %r" % body)
-
+    client.demo.bodies.insert({'body': body})
+    log.warning('got something: %s' % body)
 
 if __name__ == '__main__':
-    rmq_channel.basic_consume(handler, queue='job', no_ack=True)
-    print(' [*] Waiting for messages. To exit press CTRL+C')
+    rmq_channel.basic_consume(handler, queue='job', no_ack=False)
+    log.info('[*] Waiting for messages. To exit press CTRL+C')
     rmq_channel.start_consuming()
